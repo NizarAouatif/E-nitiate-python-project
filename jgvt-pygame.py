453ba3b02a9fb4c4998d6f2f2cf452ae89d892a8
NizarAouatif <chaclihupr@thraml.com>
2016-07-20 11:38:16 +0100

    Update jgvt-pygame.py

--- jgvt-pygame.py
+++ jgvt-pygame.py
@@ -1,16 +1,16 @@
 import pygame
 pygame.init()
 
-def screen ():
-    #screen size
-    size = [800, 520]   
-    pygame.display.set_mode(size)
-    #text dyal la barre dyal lwindow
-    pygame.display.set_caption('لعبة ديال القرد ')
-    #loop bach tsed lwindow
-    done = False
-    while not done:
-       for event in pygame.event.get(): # if the user does chi 7aja
+
+#screen size
+size = [800, 520]   
+pygame.display.set_mode(size)
+#text dyal la barre dyal lwindow
+pygame.display.set_caption('لعبة ديال القرد ')
+#loop bach tsed lwindow
+done = False
+while not done:
+    for event in pygame.event.get(): # if the user does chi 7aja
             if event.type == pygame.QUIT: # If user clicked close
                 done=True # loop is done
 
